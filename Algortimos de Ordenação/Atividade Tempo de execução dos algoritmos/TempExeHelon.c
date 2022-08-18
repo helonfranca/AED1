52ffb54432c0714196a4fc9cb620bec4699955a8
Helon de França <helonfranca02@gmail.com>
2022-08-18 20:31:13 -0300

    Alocando memória para o vetor

---
+++ Algortimos de Ordenação/Atividade Tempo de execução dos algoritmos/TempExeHelon.c
@@ -0,0 +1,235 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define MAX 100000000
+
+
+void PreenchendoVetor(int *vet,long long int n){
+    int i;
+
+    srand(time(NULL));
+
+    for(i=0; i< n; i++){
+        vet[i] = 1 + rand() % 999;
+    }
+    
+}
+
+void BubbleSort(int *vet, long long int n){
+    int i, j;
+    int aux = 0;
+
+    double time_spent = 0.0;
+
+    clock_t begin = clock();
+
+    for(j = n - 1; j > 0; j--){    
+        for(i = 0; i < j; i++){
+            if(vet[i] > vet[i+1]){
+                aux = vet[i];
+                vet[i] = vet[i+1];
+                vet[i+1] = aux;
+            }
+        }
+    }
+
+    clock_t end = clock();
+
+
+    // calcula o tempo decorrido encontrando a diferença (end - begin) e
+    // dividindo a diferença por CLOCKS_PER_SEC para converter em segundos
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+    printf("\nO tempo decorrido para ordenar o vetor com o algoritmo BubbleSort eh de %f segundos\n", time_spent); 
+}
+
+void InsertionSort(int *vet, long long int  n){
+    int i, j, aux;
+
+    double time_spent = 0.0;
+
+    clock_t begin = clock();
+
+    for (i = 1; i < n; i++){
+        aux = vet[i];
+        j = i - 1;
+    
+        while ((j>=0) && (aux < vet[j])){
+            vet[j+1] = vet[j];
+            j--;
+        }
+        vet[j+1] = aux;
+    }
+
+    clock_t end = clock();
+
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+    printf("O tempo decorrido para ordenar o vetor com o algoritmo InsertionSort eh de %f segundos\n", time_spent);
+  
+}
+
+
+
+void selection_sort(int *vet, long long int  n){
+    int i, j, min, aux;
+    
+
+    double time_spent = 0.0;
+
+    clock_t begin = clock();
+
+    for (i = 0; i < (n-1); i++) {
+        min = i;
+
+        for (j = (i+1); j < n; j++){
+            if(vet[j] < vet[min]){
+                min = j; 
+            } 
+        }   
+               
+        if(i != min){
+            aux = vet[i];
+            vet[i] = vet[min];
+            vet[min] = aux; 
+        }
+    }
+
+    clock_t end = clock();
+
+    // calcula o tempo decorrido encontrando a diferença (end - begin) e
+    // dividindo a diferença por CLOCKS_PER_SEC para converter em segundos
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+    printf("O tempo decorrido para ordenar o vetor com o algoritmo SelectionSort eh de %f segundos\n", time_spent);
+
+}
+
+void quick_sort(int *vet, int left, long long int  right){
+    int i, j, x, y;
+    
+
+    i = left;
+    j = right;
+    x = vet[(left + right) / 2];
+     
+    while(i <= j) {
+        while(vet[i] < x && i < right) {
+            i++;
+        }
+        while(vet[j] > x && j > left) {
+            j--;
+        }
+        if(i <= j) {
+            y = vet[i];
+            vet[i] = vet[j];
+            vet[j] = y;
+            i++;
+            j--;
+        }
+    }
+     
+    if(j > left) {
+        quick_sort(vet, left, j);
+    }
+    if(i < right) {
+        quick_sort(vet, i, right);
+    }   
+}
+
+void q_sort(int *vet, long long int  n){   
+    int i;
+    
+    double time_spent = 0.0;
+    clock_t begin = clock();
+    quick_sort(vet, 0, n - 1);
+    clock_t end = clock();
+    
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+
+
+    printf("O tempo decorrido para ordenar o vetor com o algoritmo QuickSort eh de %f segundos\n", time_spent);
+    
+}
+
+void shellSort(int *vet, long long int  n){
+    int i , j , value;
+    int gap = 1;
+
+    double time_spent = 0.0;
+
+    clock_t begin = clock();
+    
+    while(gap < n) {
+        gap = 3*gap+1;
+    }
+    while ( gap > 1) {
+        gap /= 3;
+        for(i = gap; i < n; i++) {
+            value = vet[i];
+            j = i - gap;
+            while (j >= 0 && value < vet[j]) {
+                vet [j + gap] = vet[j];
+                j -= gap;
+            }
+            vet [j + gap] = value;
+        }
+    }
+
+    clock_t end = clock();
+
+    // calcula o tempo decorrido encontrando a diferença (end - begin) e
+    // dividindo a diferença por CLOCKS_PER_SEC para converter em segundos
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+    printf("O tempo decorrido para ordenar o vetor com o algoritmo ShellSort eh de %f segundos\n", time_spent);
+}
+
+int main(){
+    int *vet;
+    int i;
+    int n;
+    int tmp;
+
+    do{
+        
+        printf("\t ------------------------------------------------------------ \n");
+        printf("\t|        Algortimos de Ordenacao com tempo de execucao!      |\n");
+        printf("\t|------------------------------------------------------------|\n");
+        printf("\t|      - Escolha o tamanho do vetor (ate 10000000)!          |\n");
+        printf("\t ------------------------------------------------------------ \n");
+        printf("\n\t Tamanho: "); 
+        scanf("%d",&n);
+
+        vet = (long int *) malloc(MAX*sizeof(long int));
+
+        if(vet){
+            printf("Memoria alocada!");
+        }
+
+        PreenchendoVetor(vet, n);
+        BubbleSort(vet, n);
+
+        PreenchendoVetor(vet, n);
+        selection_sort(vet, n);
+
+        PreenchendoVetor(vet, n);
+        InsertionSort(vet, n);
+
+        PreenchendoVetor(vet, n);
+        q_sort(vet, n);
+    
+        PreenchendoVetor(vet, n);
+        shellSort(vet, n);
+        
+        free(vet);
+
+        printf("\nDigite 0 para sair ou 1 para continuar: "); 
+        scanf("%d", &tmp);
+        system("cls");
+
+    }while(tmp == 1);
+
+    return 0;
+}
+
